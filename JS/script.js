c1a6dee41938d47f50d617c8927dbc79d701a592
Kauancarlos3 <carlos_kauan33@hotmail.com>
2023-03-22 01:26:37 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+class Produto {
+
+    constructor(){
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valorProduto = '';
+    }
+
+    adicionar(){
+        alert('Vamos adicionar um Produto! ');
+        // ...
+    }
+
+    deletar(){
+        alert('Item deletado');
+        //;;;;
+    }
+
+}
+
+var produto = new Produto()
